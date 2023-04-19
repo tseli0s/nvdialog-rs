16ac87d963cd16777deecc83e90fbad76f0932d0
Aggelos Tselios <aggelostselios777@gmail.com>
2023-04-19 13:01:19 +0300

    nvdialog-sys: Fix linking issues.

--- nvdialog-sys/build.rs
+++ nvdialog-sys/build.rs
@@ -3,11 +3,9 @@ extern crate bindgen;
 use std::env;
 use std::path::PathBuf;
 
-fn main() {
-    // Tell cargo to tell rustc to link the system my_library
-    // shared library.
-    println!("cargo:rustc-link-lib=my_library");
+use cmake::Config;
 
+fn main() {
     // Tell cargo to invalidate the built crate whenever the wrapper changes
     println!("cargo:rerun-if-changed=nvdialog.h");
 
@@ -26,4 +24,36 @@ fn main() {
     bindings
         .write_to_file(out_path.join("bindings.rs"))
         .expect("Couldn't write bindings!");
+
+    let dst = Config::new("./nvdialog")
+        .build_target("nvdialog")
+        .define("NVD_BUILD_STATIC", "OFF")
+        .define("NVDIALOG_MAXBUF", "256")
+        .define("NVD_USE_GTK4", "OFF")
+        .define("CMAKE_BUILD_TYPE", if cfg!(debug_assertions) { "Debug"} else { "Release" })
+        .build();
+
+    #[cfg(target_os = "linux")]
+    {
+        match pkg_config::Config::new().probe("gtk+-3.0") {
+            Ok(library) => {
+                println!("Found Gtk3, version {}.", library.version);
+                println!("Link paths: {:?}", library.link_paths);
+                println!("Link libraries: {:?}", library.libs);
+    
+                for path in library.link_paths {
+                    println!("cargo:rustc-link-search=native={}", path.display());
+                }
+                for lib in library.libs {
+                    println!("cargo:rustc-link-lib={}", lib);
+                }
+            },
+            Err(_) => {
+                panic!("You need Gtk3 on Linux to use NvDialog.")
+            },
+        }
+    }
+
+    println!("cargo:rustc-link-search=native={}/build/", dst.display());
+    println!("cargo:rustc-link-lib=nvdialog");
 }
--- nvdialog-sys/src/lib.rs
+++ nvdialog-sys/src/lib.rs
@@ -1,5 +1,7 @@
 mod ffi {
     #![allow(non_snake_case)]
     #![allow(non_camel_case_types)]
+    #![allow(non_upper_case_globals)]
+    #![allow(unused, dead_code)]
     include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 }
