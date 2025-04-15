8077bfe1da14dab1caf0614117ea39e660b2d4ea
Aggelos Tselios <aggelostselios777@gmail.com>
2025-04-15 17:25:10 +0300

    examples: Add about-dialog

---
+++ examples/about-dialog.rs
@@ -0,0 +1,24 @@
+use std::process::abort;
+use nvdialog_rs::{AboutDialog, FileDialog, FileDialogType, Image};
+
+fn main() {
+    nvdialog_rs::init().expect("failed to initialize libnvdialog");
+    let extensions = [ "png", "jpeg", "jpg", "ico", "webp" ];
+    let mut filedlg = FileDialog::new("Choose the icon for the dialog", FileDialogType::OpenFile, Some(extensions));
+    let filename = filedlg.retrieve_filename().unwrap_or_else(||{
+        eprintln!("No filename given!");
+        abort()
+    });
+    let img = Image::from_filename(filename).unwrap_or_else(|e|{
+        eprintln!("Failed to read image! {}", e.to_string());
+        abort();
+    });
+
+    let mut dialog = AboutDialog::new()
+        .name("NvDialog Example")
+        .description("An example of using nvdialog-rs and NvdAboutDialog, with a custom user-picked icon")
+        .icon(img)
+        .build();
+
+    dialog.show();
+}
